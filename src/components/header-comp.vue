3f78ede72ff4e54cfe320e5b15425faeed1ecfd0
mariokaara <mario.kaara@gmail.com>
2021-11-20 15:46:41 +0200

    header koos cssiga

--- src/components/header-comp.vue
+++ src/components/header-comp.vue
@@ -1,14 +1,43 @@
 <template>
-  <img src="src/assets/images/Color_Post_it.svg" alt="postit logo" />
+<header id="main-header">
+  <img src="../assets/images/Color_Post_it.svg" alt="postit logo" />
   <form class="search-form">
     <input type="text" />
     <input type="submit" value="Search" />
   </form>
   <div class="user-menu">
-    <img src="src/assets/images/user-circle.svg" alt="user icon" />
+    <img src="../assets/images/user-circle.svg" alt="user icon" />
     <ul>
       <li>User: Heli Helikopter</li>
       <li>E-mail: spammer@spammer.org</li>
     </ul>
   </div>
+  </header>
 </template>
+
+<style scoped>
+#main-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    padding: 5px 0;
+    background-color: white;
+    position: fixed;
+    width: 100%;
+    height: 50px;
+}
+
+#main-header img {
+    height: 50px;;
+}
+
+.search-form{
+    flex-direction: row;
+    width:35%;
+}
+
+.search-form input[type="submit"]{
+    width:100px;
+}
+</style>
