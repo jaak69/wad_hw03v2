242930b82c79fb661a8384aa8b94ec2afa64f7fb
zaiksb <peeter.niidas@ut.ee>
2021-11-20 15:40:55 +0200

    css and posts added, images fixed

--- src/components/header-comp.vue
+++ src/components/header-comp.vue
@@ -1,11 +1,11 @@
 <template>
-  <img src="src/assets/images/Color_Post_it.svg" alt="postit logo" />
+  <img src="../assets/images/Color_Post_it.svg" alt="postit logo" />
   <form class="search-form">
     <input type="text" />
     <input type="submit" value="Search" />
   </form>
   <div class="user-menu">
-    <img src="src/assets/images/user-circle.svg" alt="user icon" />
+    <img src="../assets/images/user-circle.svg" alt="user icon" />
     <ul>
       <li>User: Heli Helikopter</li>
       <li>E-mail: spammer@spammer.org</li>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <Posts msg="This is our homework #3" />
-  </div>
+  <section class="container postit-page">
+    <article class="container-post">
+      <div class="container-post-head">
+        <img src="../assets/images/user-circle.svg" alt="user icon" />
+        <p class="date">Sep 18, 2020 15:16</p>
+      </div>
+      <div class="container-post-image">
+        <img src="../assets/images/rain.png" alt="post no 1" />
+      </div>
+      <div class="container-post-comment">
+        <p class="my_caption">I think it's going to rain</p>
+        <img src="../assets/images/thumbs-up.jpeg" alt="like" />
+      </div>
+    </article>
+
+    <article class="container-post">
+      <div class="container-post-head">
+        <img src="../assets/images/user-circle.svg" alt="user icon" />
+        <p class="date">Sep 18, 2020 15:16</p>
+      </div>
+      <div class="container-post-comment">
+        <div></div>
+        <p class="my_caption">
+          Which weighs more, a pound of feathers or a pound of bricks?
+        </p>
+      </div>
+    </article>
+
+    <article class="container-post">
+      <div class="container-post-head">
+        <img src="../assets/images/user-circle.svg" alt="user icon" />
+        <p class="date">Sep 18, 2020 15:16</p>
+      </div>
+      <div class="container-post-image">
+        <img src="../assets/images/delete.png" alt="post no 3" />
+      </div>
+      <div class="container-post-comment">
+        <div class="other"></div>
+        <p class="my_caption">Felt cute, might delete later</p>
+        <img src="../assets/images/thumbs-up.jpeg" alt="like" />
+      </div>
+    </article>
+  </section>
 </template>
 
-<script>
-// @ is an alias to /src
-import Posts from "@/components/Posts.vue";
-
-export default {
-  name: "Home",
-  components: {
-    Posts,
-  },
-};
-</script>
