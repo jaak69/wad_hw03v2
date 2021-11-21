8c2067cbbacee65d51e5d894f82a52c5e09b662c
jaak69 <jk.ivask@gmail.com>
2021-11-21 17:40:04 +0200

    jaak artiklite komponent

---
+++ src/components/articlecomp.vue
@@ -0,0 +1,27 @@
+<template>
+    <article class="container-post">
+        <div class="container-post-head"> 
+            <img src="@/assets/images/user-circle.svg" alt="user icon">
+            <p v-if="post.date" class="date">{{post.date.replace('T', " ").slice(0, -8)}}</p>
+            <p v-else class="date">No datetime available</p>
+        </div>
+        <div v-if="post.post_image" class="container-post-image">
+            <img :src="post.post_image"  :alt='"post no " + post.id'>
+        </div>
+        <div v-else></div>
+        <div class="container-post-comment">
+            <p class="my_caption">{{post.caption}}</p>
+            <img src="@/assets/images/thumbs-up.jpeg" alt="like">
+        </div>
+      </article>
+</template>
+
+<script>
+export default {
+  name: "articlecomp",
+  data: function() {
+    return {
+    }
+  }
+}
+</script>
