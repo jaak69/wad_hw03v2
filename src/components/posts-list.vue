c310283599e9510d748be73a09bb98a021e511c0
mariokaara <mario.kaara@gmail.com>
2021-11-21 15:59:16 +0200

    no image in post if image missing

--- src/components/posts-list.vue
+++ src/components/posts-list.vue
@@ -7,9 +7,10 @@
             <img src="@/assets/images/user-circle.svg" alt="user icon">
             <p class="date">{{post.date}}</p>
         </div>
-        <div class="container-post-image">
+        <div v-if="post.post_image" class="container-post-image">
             <img :src="post.post_image"  :alt='"post no " + post.id'>
         </div>
+        <div v-else></div>
         <div class="container-post-comment">
             <p class="my_caption">{{post.caption}}</p>
             <img src="@/assets/images/thumbs-up.jpeg" alt="like">
